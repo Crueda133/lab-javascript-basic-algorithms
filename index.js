3cd2322a21c6da584445da5c01084e038cc65cec
Cristina Rueda <crueda133@hotmail.com>
2024-09-07 15:44:47 +0200

    lab finished

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
 // 1.3 Create a variable hacker2 with the navigator's name.
 // 1.4 Print "The navigator's name is YYYY".
 
-let hacker1 = "Paul";
+let hacker1 = "John";
 console.log(`The driver's name is ${hacker1}`);
-let hacker2 = "Steve";
+let hacker2 = "Enrique";
 console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
@@ -14,17 +14,11 @@ console.log(`The navigator's name is ${hacker2}`);
 // 2.1. Depending on which name is longer, print: - The driver has the longest name, it has XX characters. or - It seems that the navigator has the longest name, it has XX characters. - Wow, you both have equally long names, XX characters! • or
 
 if (hacker1.length > hacker2.length) {
-  console.log(
-    `The driver has the longest name, it has ${hacker1.length} characters.`,
-  );
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
 } else if (hacker1.length < hacker2.length) {
-  console.log(
-    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`,
-  );
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`);
 } else {
-  console.log(
-    `Wow, you both have equally long names, ${hacker1.length} characters!`,
-  );
+  console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
 }
 
 // Iteration 3: Loops
@@ -32,8 +26,18 @@ if (hacker1.length > hacker2.length) {
 // 3.2 Print all the characters of the navigator's name in reverse order, i.e., "nhoJ"'! "JOHN".
 // 3.3 Depending on the lexicographic order of the strings, print: • The driver's name goes first. • Yo, the navigator goes first, definitely. • What?! You both have the same name?
 
-console.log(hacker1.toUpperCase().split("").join(" "));
-console.log(hacker2.toUpperCase().split("").reverse().join(""));
+let spacedHackerName = "";
+for (let i = 0; i < hacker1.length; i++) {
+  spacedHackerName += hacker1[i].toUpperCase() + " ";
+}
+console.log(spacedHackerName.trim()); 
+
+let reverse = "";
+for (let i = hacker1.length - 1; i >= 0; i--) {
+    reverse += hacker1[i];
+}
+console.log(reverse);
+
 
 if (hacker1 < hacker2) {
   console.log("The driver's name goes first.");
@@ -43,4 +47,5 @@ if (hacker1 < hacker2) {
   console.log("What?! You both have the same name?");
 }
 
-//ask prof: Iteration 3: Loops : Why if we add the .reverse besides the hacker2 or at the end of the console.log it says that it isn't a function and give error?
+// Bonus Time!
+// Bonus 1: 
